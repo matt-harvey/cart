c023d2dbaf5cf82c1a5d90c0523cc00d9b6b5461
Matthew Harvey <software@matthewharvey.net>
2018-09-09 09:08:04 +1000

    Return if error during cart creation.

--- actions/carts.go
+++ actions/carts.go
@@ -20,11 +20,13 @@ func CreateCart(writer http.ResponseWriter, request *http.Request) {
 	if err != nil {
 		// TODO Handle this
 		Log.Print("DEBUG err: ", err)
+		return
 	}
 	err = db.Conn().Create(&cart)
 	if err != nil {
 		// TODO Handle this
 		Log.Print("DEBUG err: ", err)
+		return
 	}
 	// TODO Respond
 }
@@ -36,17 +38,20 @@ func ShowCart(writer http.ResponseWriter, request *http.Request) {
 	if err != nil {
 		// TODO Handle this
 		Log.Print("DEBUG err: ", err)
+		return
 	}
 	err = db.Conn().Find(&cart, id)
 	if err != nil {
 		// TODO Handle this
 		Log.Print("DEBUG err: ", err)
+		return
 	}
 	// TODO Handle if not found
 	json, err := json.Marshal(cart)
 	if err != nil {
 		// TODO Handle this
 		Log.Print("DEBUG err: ", err)
+		return
 	}
 	Log.Print("DEBUG json: ", string(json))
 	writer.Write([]byte(json))
